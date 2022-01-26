733c0944a9f28f6a0c9661086d38d4797738b1d1
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-01-26 23:13:42 +0600

    initialize

---
+++ grade.js
@@ -0,0 +1,23 @@
+ 
+var result = 80 ;
+if(result < 50){
+    console.log('Your friend grade is F');
+}   else if ( result >= 50 && result < 60){
+        console.log('Your friend grade is D');
+    }
+    else if( result >= 60 && result < 70){
+        console.log('Your friend grade is C');
+    }
+    else if( result >= 70 && result < 80){
+        console.log('Your friend grade is B');
+    }
+    else if( result >= 80 && result < 90){
+        console.log('Your friend grade is A');
+    }
+    else if( result >= 90 && result <= 10){
+        console.log('Your friend grade is A+');
+    }
+else{
+    console.log('Result not found ');
+}
+
